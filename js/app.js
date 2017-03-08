3ccd1fda428972bb6085fb26d3db8672f4e9bd72
Collin OConnell <ococncol@gmail.com>
2017-03-08 14:02:54 -0500

    commiting to commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,49 @@
 "use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+
+  angular
+  .module("wdinstagram", [
+    "ui.router",
+    "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .factory("EntrieFactory", [
+    "$resource",
+    EntrieFactoryFunction
+  ])
+  .controller("EntrieIndexController", [
+    "EntrieFactory",
+    EntrieIndexControllerFunction
+  ])
+
+
+function RouterFunction($stateProvider){
+  $stateProvider
+  .state("entrieIndex", {
+    url: "/entries",
+    templateUrl: "js/ng-views/index.html",
+    controller: "EntrieIndexController",
+    controllerAs: "vm"
+  })
+  .state("entrieShow", {
+    url: "/entries/:id",
+    templateUrl: "js/ng-views/show.html",
+    controller: "EntrieShowController",
+    controllerAs: "vm"
+  })
+}
+
+function EntrieFactoryFunction( $resource ){
+  return $resource( "http://localhost:3000/entries/:id")
+}
+
+function EntrieIndexControllerFunction( EntrieFactory ){
+  this.entries = EntrieFactory.query()
+}
+
+function EntrieShowControllerFunction( EntrieFactory, $stateParams ) {
+  this.entrie = EntrieFactory.get({id: $stateParams.id})
+}
